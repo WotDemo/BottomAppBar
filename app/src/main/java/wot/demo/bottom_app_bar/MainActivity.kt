ec21d6067cf64eb26014923b71cc3d5c2c18495c
yangsn <yangsn@mashang.cn>
2022-09-06 11:02:22 +0800

    [ +] init: 初始化仓库

---
+++ app/src/main/java/wot/demo/bottom_app_bar/MainActivity.kt
@@ -0,0 +1,11 @@
+package wot.demo.bottom_app_bar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
